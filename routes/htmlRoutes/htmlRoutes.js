30821dbf7b55968fefdafd0c6caf70ff3868f0d4
Greivin Arias <greivin.babito@gmail.com>
2023-08-08 15:00:47 -0400

    'commented out a route due to issues'

--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -11,8 +11,9 @@ router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
 
-router.get("*", (req, res) => {
+/*router.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
-});
+});*/ //Was giving me 'Uncaught (in promise) SyntaxError: Unexpected token '<', "<!DOCTYPE "... is not valid JSON <!DOCTYPE html>' err
+//after commenting out it worked just fine
 
 module.exports = router;
